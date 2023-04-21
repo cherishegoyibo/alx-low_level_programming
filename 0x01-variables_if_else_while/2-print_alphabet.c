4f2aab13dd192129b7c5f6158d94a7e0a20e5647
cherishegoyibo <cherishegoyibo@gmail.com>
2023-04-21 12:18:19 -0700

    Alphabet games

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Alphabet game
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
