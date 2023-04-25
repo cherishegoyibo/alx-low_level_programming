ad38b21b27e938a2955df50eff0ff55c75f821c9
cherishegoyibo <cherishegoyibo@gmail.com>
2023-04-25 13:49:21 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,12 @@
  * _isalpha - check alphabetic character
  * @c: value to be checked
  *
- * Return: 1 if letter, lowercase or uppercase, 0 else
+ * Return: 1 if letter, lowercase or uppercase,0 else
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') ||
+		(c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
