9889dd369698a003972a470c8ab8db69cc37c008
cherishegoyibo <cherishegoyibo@gmail.com>
2023-04-25 11:58:17 -0700

    void_print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print lowercase alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char aleph;
+
+	for (aleph = 'a'; aleph <= 'z'; aleph++)
+		_putchar(aleph);
+
+	_putchar('\n');
+
+}
