7ab56c312c3c8d8ad6565bbeca859dafa18b47b8
cherishegoyibo <cherishegoyibo@gmail.com>
2023-04-26 05:13:36 -0700

    absolute_value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - a program that computes absolute value of an integer
+ *  @int: integer to be computed
+ *
+ *  Return: absolute value
+ */
+int _abs(int)
+{
+	if (int >= 0)
+		return (int);
+	else
+		return (-int);
+}
