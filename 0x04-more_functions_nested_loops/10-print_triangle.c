483bfd25f14ef9dab6796b2720f20d4d9b7ee67f
cherishegoyibo <cherishegoyibo@gmail.com>
2023-04-29 08:08:52 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,22 @@ void print_triangle(int size)
 {
 	int a, b;
 
-	for (a = 1; a <= size; a++)
+	if (size <= 0)
 	{
-		for (b = 1; b <= size; b++)
-		{
-			if ((a + b) <= size)
-				_putchar(' ');
-			else
-				_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				if ((a + b) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
