7b52942f9e00f0349525faa86cb0108aa61437ca
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-02 14:15:12 -0700

    swapped

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+	*a = *b;
+	*b = i;
+}
