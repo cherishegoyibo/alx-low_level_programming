efcc14346a1b51326ed9b371cc276de715c5c539
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-03 22:07:16 -0700

    conver_atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to integer
+ * @s: input string
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1, i = 0, res = 0;
+
+	while (s[i])
+	{
+		if (s[i] == '+')
+		{
+			sign = 1;
+		}
+		else if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = (res * 10) + (s[i] - '0');
+		}
+		else if (res != 0)
+		{
+			break;
+		}
+		i++
+	}
+
+	res *= sign
+	return (res);
+}
