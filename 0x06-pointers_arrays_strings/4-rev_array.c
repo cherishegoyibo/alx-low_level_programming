217aba7473c9852c0a8b87e4936b73c2e197019c
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-04 17:55:08 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#iclude "main.h"
+/**
+ * reverse_array - reverse array
+ * @a: array
+ * @n: integer
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n--; i++)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = temp;
+	}
+}
