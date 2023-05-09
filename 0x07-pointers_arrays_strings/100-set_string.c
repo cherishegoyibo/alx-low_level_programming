1074c73ca5b282c9726e8ac29f21c225eba482f9
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-09 00:04:58 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set value of pointer to a char
+ * @s: pointer
+ * @to: char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
