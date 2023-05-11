313fd8c5b70080464836fb104b056dd9d8c61998
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-11 10:20:00 -0700

    square_root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: input
+ *
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	int nat_sqrt_recursion(int n, int sq);
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (nat_sqrt_recursion(n, sq));
+}
+/**
+ * nat_sqrt_recursion - natural square root of a number
+ * @n: input number
+ * @sq: square root
+ *
+ * Return: natural square root
+ */
+int nat_sqrt_recursion(int n, int sq)
+{
+	if (sq * sq > n)
+	{
+		return (-1);
+	}
+	if (sq * sq == n)
+	{
+		return (sq);
+	}
+	return (nat_sqrt_recursion(n, sq + 1));
+}
