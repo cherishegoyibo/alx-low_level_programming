b1410524db1f2ffdfb31f3bc873e17306b38ca1c
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-17 04:29:22 -0700

    split words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * count_word - count number of words
+ * @str: string to count
+ *
+ * Return: word count
+ */
+int count_word(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
+
+	return (count);
+}
+
+/**
+ * strtow - splits a spring into two words
+ * @str: input string
+ *
+ * Return: pointer to array of strings, else NULL
+ */
+char **strtow(char *str)
+{
+	char **word, *temp;
+	int i, j = 0, k = 0, count, strlen = 0, start, end;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	count = count_word(str);
+	word = (char **)malloc((count + 1) * (sizeof(char *)));
+	if (word == NULL)
+		return (NULL);
+
+	for (i = 0; i <= strlen; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (j)
+			{
+				end = i;
+				temp = (char *)malloc(sizeof(char) * (j + 1));
+				if (temp == NULL)
+					return (NULL);
+				while (start < end)
+					*temp++ = str[start++];
+				*temp = '\0';
+				word[k] = temp - j;
+				k++;
+				j = 0;
+			}
+		}
+		else if (j++ == 0)
+			start = i;
+	}
+
+	word[k] = NULL;
+
+	return (word);
+}
