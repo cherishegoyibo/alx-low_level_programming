444eab74f91f539df2b811eef40227623b4156a2
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-17 06:33:33 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: input argument
+ * @argv: array
+ *
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0])
+
+	return (0);
+}
