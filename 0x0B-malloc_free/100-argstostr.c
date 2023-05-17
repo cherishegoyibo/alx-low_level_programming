bf0877919c7464f39d25113c31fa037c502979d5
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-16 21:53:10 -0700

    cocatenat argumrnts

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments in a program
+ * @ac: number of arguments passsed
+ * @av: pointer to array of strings
+ *
+ * Return: pointer to a new string, NULL on fail
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, totlen = 0;
+	char *res, *temp;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, totlen++)
+			totlen++;
+	}
+	totlen++;
+
+	res = (char *)malloc((totlen + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+
+	*temp = res;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*temp = av[i][j];
+			temp++;
+		}
+		*temp = '\n';
+		*temp++;
+	}
+
+	*temp = '\0';
+	return (res);
+}
