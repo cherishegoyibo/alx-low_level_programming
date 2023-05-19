61e829eb7764377dbe3b8899d02d908ed0cb230e
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-19 08:19:03 -0700

    concat_2strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,38 +11,33 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int num = 0, len = 0, nlen, i;
+	unsigned int len1 = 0, len2 = 0, totlen, i;
 
 	if (s1 == NULL)
-	{
-		len = 0;
-	}
-	else
-	{
-		for (len = 0; s1[len]; ++len)
-			;
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		num = 0;
-	}
-	else
-	{
-		for (num = 0; s2[num]; ++num)
-			;
-	}
-	nlen = num + len;
-	if (num > n)
-		num = n;
-	p = malloc((nlen + 1) * sizeof(char));
+		s2 = "";
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	if (n >= len2)
+		n = len2;
+
+	totlen = len1 + n;
+
+	p = (char *)malloc((totlen + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < len; i++)
-		p[i] = s1[i];
-	for (i = 0; i < num; i++)
-		p[len + i] = s2[i];
 
-	p[nlen] = '\0';
+	for (i = 0; i < len1; i++)
+		p[i] = s1[i];
+
+	for (i = 0; i < n; i++)
+		p[len1 + i] = s2[i];
+
+	p[totlen] = '\0';
 
 	return (p);
 }
