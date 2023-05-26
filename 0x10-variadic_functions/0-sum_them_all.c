4091abb8f1f6ca587e5a766b386c8f1876bfdde0
cherishegoyibo <cherishegoyibo@gmail.com>
2023-05-25 21:54:22 -0700

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,13 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	unsigned int i;
 
-	va_start(numbs, n);
-
 	if (n == 0)
 		return (0);
 
 	sum = 0;
+	
+	va_start(numbs, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(numbs, int);
 
