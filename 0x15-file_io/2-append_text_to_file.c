16395916499ae5425a6811ebf5fda14104637119
cherishegoyibo <cherishegoyibo@gmail.com>
2023-06-13 15:13:11 -0700

    task2 update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,9 +16,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
-
 	fadd = fopen(filename, "a");
 
 	if (fadd == NULL)
