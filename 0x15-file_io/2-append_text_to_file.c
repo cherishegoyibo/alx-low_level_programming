1e08ba0c8576c3ff297a4772879d5718866f6eeb
cherishegoyibo <cherishegoyibo@gmail.com>
2023-09-04 13:34:27 -0700

    file_io_tasks

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: content to append into the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *fadd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fadd = fopen(filename, "a");
+
+	if (fadd == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		fputs(text_content, fadd);
+
+	fclose(fadd);
+	return (1);
+}
