f482cbf042173ab67eb75fdfc34e1e24b13d5909
cherishegoyibo <cherishegoyibo@gmail.com>
2023-10-12 12:08:37 -0700

    mandatory task

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * print_dlistint - prints all the element of a list
+ * @h: head of list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+# 0x17. C - Doubly linked lists
+This repo contains solution to tasks done on project 0x17. C - Doubly linked lists
