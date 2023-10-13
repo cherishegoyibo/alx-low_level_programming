dd8a7867993c7a8cadbcc6f80629af0f55ee12db
cherishegoyibo <cherishegoyibo@gmail.com>
2023-10-12 19:27:25 -0700

    mandatory task

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums all of the data(n) of a linked list
+ * @head: pointer to the beginning of the list
+ *
+ * Return: sum of all data,else 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head->prev != NULL)
+		head = head->prev;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
