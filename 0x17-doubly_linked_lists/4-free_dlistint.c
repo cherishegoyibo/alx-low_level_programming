85c4b61d06648dac2541c2d57d70c7484431c718
cherishegoyibo <cherishegoyibo@gmail.com>
2023-10-12 18:14:36 -0700

    mandatory task

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a double linked list
+ * @head: pointer to begining of list
+ *
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
