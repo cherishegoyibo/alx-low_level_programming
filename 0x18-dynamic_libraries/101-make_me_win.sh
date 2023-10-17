f754228c96782944614345187ba9a2ed508d535e
cherishegoyibo <cherishegoyibo@gmail.com>
2023-10-16 20:59:13 -0700

    advance task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/cherishegoyibo/alx-low_level_programming/raw/main/0x18-dynamic_libraries/cher.so
+wget -P /tmp https://github.com//alx-low_level_programming/raw/master/0x18-dynamic_libraries/cher.so
 export LD_PRELOAD=/tmp/cher.so
