11b1d0ce2d831008d03bb821fda275c165e0e2b5
Cher <128803211+cherishegoyibo@users.noreply.github.com>
2024-02-04 12:28:19 +0100

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp https://github.com//alx-low_level_programming/raw/master/0x18-dynamic_libraries/cher.so
-export LD_PRELOAD=/tmp/cher.so
--- 0x18-dynamic_libraries/README.md
+++
@@ -1,2 +0,0 @@
-# 0x18. C - Dynamic libraries
-Contains solution to task carried out on project 0x18. C - Dynamic libraries
